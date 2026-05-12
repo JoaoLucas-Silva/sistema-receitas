f334338ce86960a6a49e0557d6749b17e8a088c7
Joao Lucas <silva.joaolucas@outlook.com.br>
2026-05-12 18:36:19 -0300

    feat: rotas para categoria

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/auth');
 
 const controllerUsuario = require('../controllers/controllerUsuario');
+const controllerCategoria = require('../controllers/controllerCategoria');
 
 router.post('/login', controllerUsuario.postLogin);
 router.get('/logout', controllerUsuario.getLogout);
 
-const auth = require('../middlewares/auth');
-
 router.get('/home', auth.isAuthenticated, (req, res) => {
   return res.send('Bem-vindo!');
 });
+
+router.post('/categoria', controllerCategoria.createCategoria);
+router.get('/categoria', controllerCategoria.getCategorias);
+router.get('/categoria/:id', controllerCategoria.getCategoriaById);
+router.put('/categoria/:id', controllerCategoria.updateCategoria);
+router.delete('/categoria/:id', controllerCategoria.deleteCategoria);
 
 module.exports = router;
